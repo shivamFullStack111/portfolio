502f11ba2366534b7b99a17c9eba589bc0c48ab4
shivamo7s <shivam@o7tec.com>
2025-06-03 10:00:03 +0530

    add some projects in projects page

--- frontend/src/screens/projects/Projects.tsx
+++ frontend/src/screens/projects/Projects.tsx
@@ -26,8 +26,8 @@ const Projects = () => {
         <div className="flex w-full justify-center "></div>
       </div>
 
-      <div className="flex px-14 mt-12 gap-7 ">
-        <div className="w-1/3 h-[310px] ">
+      <div className="flex px-14 mt-12 flex-wrap gap-7 ">
+        <div className="w-[31.5%] h-[310px] ">
           <img
             src="chat-fussion.png"
             alt=""
@@ -36,67 +36,107 @@ const Projects = () => {
           <div className="p-1">
             <p className="font-semibold text-[#E6FF00]">Chat Fussion</p>
             <p className="text-gray-300 text-[12px] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-              fugit et?{" "}
+              A feature-rich chat app where users can send messages, make
+              audio/video calls, share location, images, and videos, and
+              customize settings like notifications and profile picture.
             </p>
             <div className="flex justify-end p-2">
-              <span>
+              <Link
+                target="_blank"
+                to={
+                  "https://chat-fussion-chat-app-mern-stack-with-best-practices-s1ed.vercel.app/"
+                }
+              >
                 <CustomButton
                   className="text-sm px-5"
                   onClick={() => {}}
                   title="Live Preview"
                 />
-              </span>
+              </Link>
             </div>
           </div>
         </div>
-        <div className="w-1/3 h-[310px] ">
+        <div className="w-[31.5%] h-[310px] ">
           <img
-            src="chat-fussion.png"
+            src="ai-resume-builder.png"
             alt=""
             className="h-[200px]  w-full border border-gray-500"
           />
           <div className="p-1">
-            <p className="font-semibold text-[#E6FF00]">Chat Fussion</p>
+            <p className="font-semibold text-[#E6FF00]">Ai Resume Builder</p>
             <p className="text-gray-300 text-[12px] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-              fugit et?{" "}
+              A powerful resume builder with attractive, customizable templates
+              — powered by Gemini AI for personalized suggestions based on your
+              profile.{" "}
             </p>
             <div className="flex justify-end p-2">
-              <span>
+              <Link
+                target="_blank"
+                to={
+                  "https://ai-resume-builder-fi4lnpa6u-shivamfullstack111s-projects.vercel.app/?page=4&templateid=67dea709e90236da68930848&edit=experience-details"
+                }
+              >
                 <CustomButton
                   className="text-sm px-5"
                   onClick={() => {}}
                   title="Live Preview"
                 />
-              </span>
+              </Link>
             </div>
           </div>
         </div>
-        <div className="w-1/3 h-[310px] ">
+
+        <div className="w-[31.5%] h-[310px] ">
           <img
-            src="chat-fussion.png"
+            src="job-portal.png"
             alt=""
             className="h-[200px]  w-full border border-gray-500"
           />
           <div className="p-1">
-            <p className="font-semibold text-[#E6FF00]">Chat Fussion</p>
+            <p className="font-semibold text-[#E6FF00]">Job Portal</p>
             <p className="text-gray-300 text-[12px] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-              fugit et?{" "}
+              An attractive job portal landing page built with smooth Framer
+              Motion animations for a dynamic user experience.{" "}
             </p>
             <div className="flex justify-end p-2">
-              <span>
+              <Link
+                target="_blank"
+                to={"https://job-portal-website-seven.vercel.app/"}
+              >
                 <CustomButton
                   className="text-sm px-5"
                   onClick={() => {}}
                   title="Live Preview"
                 />
-              </span>
+              </Link>
             </div>
           </div>
         </div>
-       
+        <div className="w-[31.5%] h-[310px] ">
+          <img
+            src="urban-cart.png"
+            alt=""
+            className="h-[200px]  w-full border border-gray-500"
+          />
+          <div className="p-1">
+            <p className="font-semibold text-[#E6FF00]">Urban Cart</p>
+            <p className="text-gray-300 text-[12px] italic">
+              Complete e-commerce platform with advanced custom filtering.
+            </p>
+            <div className="flex justify-end p-2">
+              <Link
+                target="_blank"
+                to={"https://urban-cart-nextjs.vercel.app/"}
+              >
+                <CustomButton
+                  className="text-sm px-5"
+                  onClick={() => {}}
+                  title="Live Preview"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer />
