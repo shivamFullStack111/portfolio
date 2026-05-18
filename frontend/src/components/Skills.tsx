b41df32fef8d745a46f0a9a13ff5aaff862cb996
shivam <shvam12340987@gmail.com>
2026-05-18 22:46:03 +0530

    add langchain image in home page

--- frontend/src/components/Skills.tsx
+++ frontend/src/components/Skills.tsx
@@ -14,8 +14,9 @@ const Skills = () => {
       <Marquee>
         <div className="flex gap-16 md:gap-26  lg:gap-40 mt-14 overflow-x-auto">
           <img alt="" src="react.png" className="h-16 md:h-20   lg:h-20"></img>
-          <img alt="" src="angular.png" className="h-16 md:h-20   lg:h-20"></img>
+          {/* <img alt="" src="angular.png" className="h-16 md:h-20   lg:h-20"></img> */}
           <img alt="" src="node.png" className="h-16 md:h-20   lg:h-20"></img>
+          <img alt="" src="langchain.png" className="h-16 md:h-20 invert  lg:h-20"></img>
           <img alt="" src="mongodb.png" className="h-16 md:h-20   lg:h-20"></img>
           <img alt="" src="reactNative.png" className="h-16 md:h-20   lg:h-20"></img>
           <img alt="" src="redux.png" className="h-16 md:h-20   lg:h-20"></img>
